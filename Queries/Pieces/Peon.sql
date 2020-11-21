963e2c93899dac6e189d72a17f53969907d4d8ff
Andrés Bonilla <andres.bonilla1298@gmail.com>
2020-11-21 11:08:21 -0600

    Primera versión del objeto PIECE, el cual se va a utilizar para el manejo de los atributos de las piezas

--- Queries/Pieces/Peon.sql
+++ Queries/Pieces/Peon.sql
@@ -15,55 +15,50 @@ IS
 	CHECKPOS VARCHAR2(10);
 BEGIN
 
-        COLUMN1 := COLUMNTONUMBER(LOWER(SUBSTR(source, 1, 1)));
-		COLUMN2 := COLUMNTONUMBER(LOWER(SUBSTR(target, 1, 1)));
-		ROW1 := TO_NUMBER(SUBSTR(source, 2, 2));
-		ROW2 := TO_NUMBER(SUBSTR(target, 2, 2));
-		IF(matchTurn=1) then
+    COLUMN1 := COLUMNTONUMBER(LOWER(SUBSTR(source, 1, 1)));
+    COLUMN2 := COLUMNTONUMBER(LOWER(SUBSTR(target, 1, 1)));
+    ROW1 := TO_NUMBER(SUBSTR(source, 2, 2));
+    ROW2 := TO_NUMBER(SUBSTR(target, 2, 2));
+    IF(matchTurn=1) then
         DBMS_OUTPUT.PUT_LINE('blanco');
 
 
-            IF(targetData <> 'EMPTY'
-                   AND ((ROW1+1)=ROW2)
-                   AND ((COLUMN1 + 1) = COLUMN2) OR (COLUMN1 - 1) = COLUMN2) THEN
+        IF(targetData <> 'EMPTY' AND ((ROW1+1)=ROW2) AND ((COLUMN1 + 1) = COLUMN2) OR (COLUMN1 - 1) = COLUMN2) THEN
             DBMS_OUTPUT.PUT_LINE('Diagonal');
 
             DELETE FROM PIECES_PER_MATCH WHERE MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN2) AND "ROW" = ROW2;
             UPDATE PIECES_PER_MATCH SET "COLUMN" = NUMBERTOCOLUMN(COLUMN2), "ROW" = ROW2 where MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN1) AND "ROW" = ROW1;
             RETURN TRUE;
-            END IF;
+        END IF;
 
 
+        CHECKPOS := UPPER(NUMBERTOCOLUMN(COLUMN1)) || '' || (ROW1+1);
+        CHECKPOS := TargetPosition(CHECKPOS, matchTurn, matchID);
 
-        CHECKPOS := UPPER(NUMBERTOCOLUMN(COLUMN1))||''||(ROW1+1);
-		CHECKPOS := TargetPosition(CHECKPOS, matchTurn, matchID);
-            IF(CHECKPOS = 'EMPTY' AND COLUMN1 = COLUMN2 AND (ROW1 + 1 = ROW2 OR ((ROW1 = 2) AND ((ROW1 + 2 = ROW2) OR (ROW1 + 1 = ROW2))))) then
-                UPDATE PIECES_PER_MATCH SET "COLUMN" = NUMBERTOCOLUMN(COLUMN2), "ROW" = ROW2 where MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN1) AND "ROW" = ROW1;
-                RETURN TRUE;
-            END IF;
-        ELSE
+        IF(CHECKPOS = 'EMPTY' AND COLUMN1 = COLUMN2 AND (ROW1 + 1 = ROW2 OR ((ROW1 = 2) AND ((ROW1 + 2 = ROW2) OR (ROW1 + 1 = ROW2))))) then
+            UPDATE PIECES_PER_MATCH SET "COLUMN" = NUMBERTOCOLUMN(COLUMN2), "ROW" = ROW2 where MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN1) AND "ROW" = ROW1;
+            RETURN TRUE;
+        END IF;
+    ELSE
 
-
-            IF(targetData <> 'EMPTY'
-                   AND ((ROW1-1)=ROW2)
-                   AND ((COLUMN1 - 1) = COLUMN2) OR (COLUMN1 + 1) = COLUMN2) THEN
+        IF(targetData <> 'EMPTY' AND ((ROW1-1)=ROW2) AND ((COLUMN1 - 1) = COLUMN2) OR (COLUMN1 + 1) = COLUMN2) THEN
             DBMS_OUTPUT.PUT_LINE('Diagonal');
 
             DELETE FROM PIECES_PER_MATCH WHERE MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN2) AND "ROW" = ROW2;
             UPDATE PIECES_PER_MATCH SET "COLUMN" = NUMBERTOCOLUMN(COLUMN2), "ROW" = ROW2 where MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN1) AND "ROW" = ROW1;
             RETURN TRUE;
-            END IF;
+        END IF;
 
 
         CHECKPOS := UPPER(NUMBERTOCOLUMN(COLUMN1))||''||(ROW1-1);
-		CHECKPOS := TargetPosition(CHECKPOS, matchTurn, matchID);
-                DBMS_OUTPUT.PUT_LINE('negro');
-            IF(CHECKPOS = 'EMPTY' AND COLUMN1 = COLUMN2 AND (ROW1 - 1 = ROW2 OR ((ROW1 = 7) AND ((ROW1 - 2 = ROW2)) OR (ROW1 - 1 = ROW2)))) then
-                UPDATE PIECES_PER_MATCH SET "COLUMN" = NUMBERTOCOLUMN(COLUMN2), "ROW" = ROW2 where MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN1) AND "ROW" = ROW1;
-                RETURN TRUE;
-            END IF;
+        CHECKPOS := TargetPosition(CHECKPOS, matchTurn, matchID);
+        DBMS_OUTPUT.PUT_LINE('negro');
+        IF(CHECKPOS = 'EMPTY' AND COLUMN1 = COLUMN2 AND (ROW1 - 1 = ROW2 OR ((ROW1 = 7) AND ((ROW1 - 2 = ROW2)) OR (ROW1 - 1 = ROW2)))) then
+            UPDATE PIECES_PER_MATCH SET "COLUMN" = NUMBERTOCOLUMN(COLUMN2), "ROW" = ROW2 where MATCHES_ID = matchID AND "COLUMN" = NUMBERTOCOLUMN(COLUMN1) AND "ROW" = ROW1;
+            RETURN TRUE;
         END IF;
+    END IF;
 
-        RETURN FALSE;
+    RETURN FALSE;
 END;
 /
